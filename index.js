8faf7e54729a5f8919484a9a4a28e512d77845c1
Joseph Crump <josephcrump79@yahoo.com>
2024-09-04 17:24:48 -0500

    phase-1-scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution in this file!
+var customerName = 'bob'
+//var bestCustomer = 'fender' 
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'poop' 
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'pee'
+}
